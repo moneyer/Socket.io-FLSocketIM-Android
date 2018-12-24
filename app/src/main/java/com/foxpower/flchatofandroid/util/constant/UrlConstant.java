4e9d161f3f46b9414a9e6559e5ee3681d2473662
moneyer <zhichao_java@163.com>
2018-12-24 10:51:40 +0800

    first commit

---
+++ app/src/main/java/com/foxpower/flchatofandroid/util/constant/UrlConstant.java
@@ -0,0 +1,17 @@
+package com.foxpower.flchatofandroid.util.constant;
+
+/**
+ * Created by fengli on 2018/2/5.
+ */
+
+public class UrlConstant {
+
+//    public static final String baseUrl = "http://172.30.1.89:3000";
+    //test baseUrl = "http://192.168.232.121:3000"
+public static final String baseUrl = "http://192.168.43.17:3000";
+    public static final String login_url = "mobileLogin";
+    public static final String register_url = "register";
+    public static final String allUsers_url = "allUsers";
+
+    public static String auth_token = "";
+}
